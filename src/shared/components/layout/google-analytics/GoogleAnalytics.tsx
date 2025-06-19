d6119f3522087a5f8f4a0b93c4148dd372e96d5c
Petr Lipatov <lipatov3@gmail.com>
2025-06-19 18:01:23 +0300

    fix: Image added

---
+++ src/shared/components/layout/google-analytics/GoogleAnalytics.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+import Script from "next/script";
+import { GA_ID } from "@/shared/constants/conf";
+
+export function GoogleAnalyticsWithTracker() {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    window.gtag("config", GA_ID, {
+      page_path: pathname,
+    });
+  }, [pathname]);
+
+  return (
+    <>
+      {/* Загрузка библиотеки gtag.js */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      {/* Инициализация dataLayer и первого pageview */}
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}', { page_path: window.location.pathname });
+        `}
+      </Script>
+    </>
+  );
+}
---
+++ src/shared/constants/conf.ts
@@ -0,0 +1,1 @@
+export const GA_ID = "G-Z45CB72BS5";
