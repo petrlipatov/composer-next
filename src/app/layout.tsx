63870febe71b35314d49190a8890c91d08205a91
Petr Lipatov <lipatov3@gmail.com>
2025-06-19 19:23:11 +0300

    fix: Image added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { ViewportHeight } from "@/shared/components/layout/viewport-height";
 import { Providers } from "@/shared/providers";
 import { ReactNode, Suspense } from "react";
@@ -6,11 +6,16 @@ import { UrlSync } from "@/feature/url-synchronizer/UrlSync";
 import "@/shared/styles/globals.css";
 import { GoogleAnalytics } from "@/shared/components/layout/google-analytics/GoogleAnalytics";
 
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 export const metadata: Metadata = {
   title: "Liza Tikhonova",
   description: "Music Composer",
-  viewport:
-    "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
   openGraph: {
     type: "website",
     url: "https://lizatikhonova.com/",
