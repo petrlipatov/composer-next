06034fc367d46ab04f6cb1759b6995675d19b58a
Petr Lipatov <lipatov3@gmail.com>
2025-08-30 10:14:07 +0300

    fix: Reload on orientation change removed

--- src/shared/components/layout/viewport-controller/ViewportController.tsx
+++ src/shared/components/layout/viewport-controller/ViewportController.tsx
@@ -2,23 +2,10 @@
 
 import { useDeviceType } from "@/shared/hooks/useDeviceType";
 import { usePageDynamicHeight } from "@/shared/hooks/usePageDynamicHeight";
-import { useEffect } from "react";
 
 export const ViewportController = () => {
   usePageDynamicHeight();
   useDeviceType();
-
-  useEffect(() => {
-    const handleOrientationChange = () => {
-      window.location.reload();
-    };
-
-    window.addEventListener("orientationchange", handleOrientationChange);
-
-    return () => {
-      window.removeEventListener("orientationchange", handleOrientationChange);
-    };
-  }, []);
 
   return null;
 };
